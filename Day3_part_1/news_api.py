094ebb0834ec8f15b3fea19443046f101e7036c6
Sukhov Pavel <pasukhov@yandex-team.ru>
2018-01-26 15:44:44 +0300

    initial commit

---
+++ Day3_part_1/news_api.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# https://newsapi.org/
+
+import urllib2 as url2
+import urllib as url
+import json
+
+BASE_URL = "https://newsapi.org/v1/"
+METHOD = "articles"
+PARAMS = {
+    "source": "cnn",
+    "apiKey": "???"
+}
+
+# res = url2.urlopen(BASE_URL + METHOD + "?" + url.urlencode(PARAMS)).read()
+
+"""
+for a in json.loads(res)["articles"][:3]:
+    print
+    print "Title: " + a["title"]
+    print "Description: " + a["description"]
+    print "Link: " + a["url"]
+    print
+"""
+
+def get_news(source="cnn"):
+    return None
